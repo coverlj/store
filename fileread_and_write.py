1c91ea82edffbcb5214f34413ea1a20eba075778
coverlj <xxx>
2021-12-20 17:35:33 +0800

    最终版本

--- fileread_and_write.py
+++ fileread_and_write.py
@@ -6,7 +6,6 @@ for line in f1.readlines():
     line=line.strip("\n")
     line=line.split()
     data.append(line)
-print(data)
 while True:
     for i in range(len(data)):
         data1[data[i][0]] = 0
@@ -14,10 +13,10 @@ while True:
             if data[i][0]==data[y][0]:
                 continue
             else:
-                data1[data[i][0]] = 0
+                data1[data[i][0]] = 1
     for i in data1:
         for y in range(len(data)):
             if i==data[y][0]:
                 data1[i]+=1
         print("用户ip为：",i,"访问了",data1[i],"次")
-    time.sleep(5)
+    time.sleep(30)
