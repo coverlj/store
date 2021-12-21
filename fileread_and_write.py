556ca4c09fb7d46995895c3b7b029b93e49728c6
coverlj <xxx>
2021-12-21 11:43:37 +0800

    4

--- fileread_and_write.py
+++ fileread_and_write.py
@@ -1,6 +1,7 @@
 import time
-f1 = open(file="baidu_x_system.txt",mode="r+",encoding="utf-8")
-data = []
+f1=open(file="baidu_x_system.log",mode="r+",encoding="utf-8")
+f2=open(file="d:\\rzgx.txt",mode="w+",encoding="utf-8")
+data=[]
 data1={}
 for line in f1.readlines():
     line=line.strip("\n")
@@ -9,14 +10,13 @@ for line in f1.readlines():
 while True:
     for i in range(len(data)):
         data1[data[i][0]] = 0
-        for y in range(1,len(data)-1):
-            if data[i][0]==data[y][0]:
-                continue
-            else:
-                data1[data[i][0]] = 1
     for i in data1:
         for y in range(len(data)):
             if i==data[y][0]:
                 data1[i]+=1
-        print("用户ip为：",i,"访问了",data1[i],"次")
+        f2.write("用户ip是：")
+        f2.write(i)
+        f2.write("\n访问次数是：")
+        f2.write(str(data1[i]))
+        f2.write("\n")
     time.sleep(30)
